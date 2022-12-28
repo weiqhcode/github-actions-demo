4c25c8b045244901093e42131d63def1ddb9d156
weiqhcode <weiqh2000@163.com>
2022-12-28 18:21:18 +0800

    第一次提交代码，不含workflow文件

---
+++ src/test/java/net/weiqh/githubactionsdemo/GithubActionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.weiqh.githubactionsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
